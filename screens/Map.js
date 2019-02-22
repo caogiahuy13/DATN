7bc9cb7b2507b0bd1bf1b7b19be9f1cf0a26e48d
Cao Gia Huy <caogiahuy13@gmail.com>
2019-02-22 20:34:15 +0700

    Add map to Map.js

--- screens/Map.js
+++ screens/Map.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default class Setting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-          <Text style={styles.text}> This is MAP Tab </Text>
-      </View>
+      <MapView style={styles.map}
+          initialRegion={{
+            latitude: 10.762864,
+            longitude: 106.682229,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+        }}>
+            <Marker coordinate={{
+              latitude: 10.762864,
+              longitude: 106.682229,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,}}
+            >
+            </Marker>
+        </MapView>
+
+
     );
   }
 }
@@ -19,5 +34,8 @@ const styles = StyleSheet.create({
     },
     text: {
       fontSize: 20,
-    }
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+      },
 })
