8f2fed2c3b7eeecb92dbbd7cc9c38afbae69e339
your username <caogiahuy13@gmail.com>
2019-03-03 16:43:43 +0700

    Create some element form Setting Screens

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Tours from './src/screens/Tours';
+import Setting from './src/screens/Setting';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Setting);
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Alert } from 'react-native';
+import { ListItem, Divider, Avatar, Icon } from 'react-native-elements'
 
 export default class Setting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-          <Text style={styles.text}> This is SETTING Tab </Text>
-      </View>
+      <ScrollView style={styles.scroll}>
+        <View style={styles.userRow}>
+          <View style={styles.userImage}>
+            <Avatar
+              rounded
+              size="large"
+              source={{
+                uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+              }}
+            />
+          </View>
+          <View>
+            <Text style={{ fontSize: 16 }}>Cao Gia Huy</Text>
+            <Text
+              style={{
+                color: 'gray',
+                fontSize: 14,
+              }}
+            >
+              "caogiahuy13@gmail.com"
+            </Text>
+          </View>
+        </View>
+
+        <InfoText text="Account"/>
+
+        <ListItem
+            // chevron
+            title="Currency"
+            rightTitleStyle={{ fontSize: 15 }}
+            onPress={() => {Alert.alert("ABC")}}
+            containerStyle={styles.listItemContainer}
+            leftIcon={
+              <Icon
+                name='log-out'
+                type='entypo'
+                color='#00aced' />
+            }
+            rightIcon={<Chevron />}
+          />
+      </ScrollView>
     );
   }
 }
+
+const InfoText = ({ text }) => (
+  <View style={styles.containerInfoText}>
+    <Text style={styles.infoText}>{text}</Text>
+  </View>
+)
+
+const Chevron = () => (
+  <Icon
+    name="chevron-right"
+    type="entypo"
+    containerStyle={{ marginLeft: -15, width: 20 }}
+    color='#D1D1D6'
+  />
+)
 
 const styles = StyleSheet.create({
     container: {
@@ -19,5 +73,35 @@ const styles = StyleSheet.create({
     },
     text: {
       fontSize: 20,
-    }
+    },
+    containerInfoText: {
+      paddingTop: 20,
+      paddingBottom: 12,
+      backgroundColor: '#F4F5F4',
+    },
+    infoText: {
+      fontSize: 16,
+      marginLeft: 20,
+      color: 'gray',
+      fontWeight: '500',
+    },
+    userRow: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      paddingBottom: 8,
+      paddingLeft: 15,
+      paddingRight: 15,
+      paddingTop: 6,
+    },
+    userImage: {
+      marginRight: 12,
+    },
+    listItemContainer: {
+      height: 55,
+      borderWidth: 0.5,
+      borderColor: '#ECECEC',
+    },
+    scroll: {
+      backgroundColor: 'white',
+    },
 })
