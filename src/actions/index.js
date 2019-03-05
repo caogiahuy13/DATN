1937ee79f4da36c57e0777c894ad3b248c4f0b8d
your username <caogiahuy13@gmail.com>
2019-03-05 13:19:35 +0700

    Update reducers, actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,5 @@
 import * as types from './type.js';
 
-export function increment(){
-  return{
-    type: types.INCREMENT,
-  };
-}
-export function decrement(){
-  return{
-    type: types.DECREMENT
-  };
-}
-
 export function changeCurrentLocation(lat, lng){
   return {
     type: types.CHANGE_CURRENT_LOCATION,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux';
-import countReducer from './countReducer.js';
 import regionReducer from './regionReducer';
 import nearLocationReducer from './nearLocationReducer';
 
 const allReducers= combineReducers({
-  count: countReducer,
   region: regionReducer,
   nearLocation: nearLocationReducer,
 });
