146ccd5648db5d032e2c322e0212f7f9c0676d7a
your username <caogiahuy13@gmail.com>
2019-03-07 18:57:41 +0700

    Update TourCarousel and TourCarouselEntry

--- src/components/TourCarousel.js
+++ src/components/TourCarousel.js
@@ -5,7 +5,7 @@ import {connect} from 'react-redux';
 import Carousel from 'react-native-snap-carousel';
 import { Button, Icon } from 'react-native-elements';
 
-import {handleModalLocation, handleTourCarousel, changeCurrentRoute} from '../actions/index.js';
+import {handleModalLocation, handleTourCarousel} from '../actions/index.js';
 
 import TourCarouselEntry from './TourCarouselEntry';
 
@@ -13,54 +13,10 @@ const window = Dimensions.get('window');
 const { width, height }  = window;
 
 class TourCarousel extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  _getRouteByTour(){
-    return fetch('http://localhost:5000/route/getByTour/4')
-              .then((response) => response.json())
-              .then((responseJson) => {
-                  this.props.changeCurrentRoute(responseJson.data);
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-  }
-
-  _onDirectionPress(e){
-    console.log("TEST");
-  }
-
   // render tour carousel tương ứng với tour đi qua địa điểm
   _renderItem ({item, index}) {
       return (
-          <View style={styles.container}>
-            <TourCarouselEntry/>
-            {/*<Image source={require("../assets/images/tour-card-img.jpg")} style={styles.image}/>
-            <View style={{paddingHorizontal: 6, paddingBottom: 7}}>
-              <Text onPress={this.test()} style={styles.text}>{item.name}</Text>
-              <View style={{flexDirection: 'row'}}>
-                <Button
-                  icon={<Icon name="directions" type="materialicons" size={18} color="white" iconStyle={{marginRight: 4}}/>}
-                  type="solid"
-                  title="Direction"
-                  titleStyle={{fontSize: 14}}
-                  buttonStyle={styles.button}
-                  onPress={()=>{console.log("TEST")}}
-                  onLongPress={()=>{console.log("TEST2")}}
-                />
-                <Button
-                  icon={<Icon name="rightcircle" type="antdesign" size={16} color="#2089DC" iconStyle={{marginRight: 4}}/>}
-                  type="outline"
-                  title="Detail"
-                  titleStyle={{fontSize: 14}}
-                  buttonStyle={styles.button}
-                  onPress={()=>{}}
-                />
-              </View>
-            </View>*/}
-          </View>
+          <TourCarouselEntry item={item}/>
       );
   }
 
@@ -80,45 +36,15 @@ class TourCarousel extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    elevation: 2,
-    borderRadius: 16,
-    borderWidth: 0.1,
-    borderColor: 'black',
-  },
-  image: {
-    width: undefined,
-    height: 64,
-    justifyContent: 'flex-end',
-    borderTopRightRadius: 16,
-    borderTopLeftRadius: 16,
-  },
-  text: {
-    padding: 4,
-    fontWeight: 'bold',
-    shadowColor: 'black',
-  },
-  button: {
-    borderRadius: 10,
-    paddingVertical: 2,
-    margin: 1,
-    alignSelf: 'flex-start'
-  },
-});
-
 function mapStateToProps(state){
   return{
     modalLocation: state.modalLocation,
-    currentRoute: state.currentRoute,
   };
 }
 function mapDispatchToProps(dispatch){
   return bindActionCreators({
     handleModalLocation: handleModalLocation,
     handleTourCarousel: handleTourCarousel,
-    changeCurrentRoute: changeCurrentRoute,
   }, dispatch)
 }
 
--- src/components/TourCarouselEntry.js
+++ src/components/TourCarouselEntry.js
@@ -1,19 +1,95 @@
 import React, { Component } from 'react';
-import {Text, View, Image, StyleSheet, Dimensions, ImageBackground} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import Carousel from 'react-native-snap-carousel';
 import { Button, Icon } from 'react-native-elements';
 
-export default class TourCarouselEntry extends Component {
+import {changeCurrentRoute} from '../actions/index.js';
 
-    testfunc(){
-      console.log("TEST2");
-    }
+class TourCarouselEntry extends Component {
+  _getRouteByTour(){
+    return fetch('http://localhost:5000/route/getByTour/4')
+              .then((response) => response.json())
+              .then((responseJson) => {
+                  this.props.changeCurrentRoute(responseJson.data);
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+  }
 
-    render () {
-        return (
-          <Button title="TEST" onPress={()=>{this.testfunc()}}/>
-        )
-    }
+  _onDirectionPress(e){
+    console.log("TEST");
+  }
+
+  render () {
+    const {item} = this.props;
+    return (
+      <View style={styles.container}>
+        <Image source={require("../assets/images/tour-card-img.jpg")} style={styles.image}/>
+        <View style={{paddingHorizontal: 6, paddingBottom: 7}}>
+          <Text style={styles.text}>{item.name}</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Button
+              icon={<Icon name="directions" type="materialicons" size={18} color="white" iconStyle={{marginRight: 4}}/>}
+              type="solid"
+              title="Direction"
+              titleStyle={{fontSize: 14}}
+              buttonStyle={styles.button}
+              onPress={()=>{this._onDirectionPress()}}
+            />
+            <Button
+              icon={<Icon name="rightcircle" type="antdesign" size={16} color="#2089DC" iconStyle={{marginRight: 4}}/>}
+              type="outline"
+              title="Detail"
+              titleStyle={{fontSize: 14}}
+              buttonStyle={styles.button}
+              onPress={()=>{}}
+            />
+          </View>
+        </View>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    elevation: 2,
+    borderRadius: 16,
+    borderWidth: 0.1,
+    borderColor: 'black',
+  },
+  image: {
+    width: undefined,
+    height: 64,
+    justifyContent: 'flex-end',
+    borderTopRightRadius: 16,
+    borderTopLeftRadius: 16,
+  },
+  text: {
+    padding: 4,
+    fontWeight: 'bold',
+    shadowColor: 'black',
+  },
+  button: {
+    borderRadius: 10,
+    paddingVertical: 2,
+    margin: 1,
+    alignSelf: 'flex-start'
+  },
+});
+
+function mapStateToProps(state){
+  return{
+    currentRoute: state.currentRoute,
+  };
+}
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({
+    changeCurrentRoute: changeCurrentRoute,
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TourCarouselEntry);
