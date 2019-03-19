1de89d1456dc53e457bae111e4d0b7f03e509802
your username <caogiahuy13@gmail.com>
2019-03-19 18:57:04 +0700

    BOOKING_INFO: add contact and passenger info

--- src/screens/BookingInfo.js
+++ src/screens/BookingInfo.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Dimensions, TextInput, Touch, TouchableOpacity } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { View, StyleSheet, Text, Dimensions, TextInput, Touch, TouchableOpacity, ScrollView, Keyboard, TouchableWithoutFeedback, NativeModules } from 'react-native';
+import { Card, Icon, ListItem } from 'react-native-elements';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import Modal from 'react-native-modal';
 
 import { COLOR_MAIN, COLOR_GRAY_BACKGROUND } from '../constants/index';
 
-const deviceWidth = Dimensions.get("window").width;
-
 import BookingStage from '../components/BookingStage';
 import NumberPicker from '../components/NumberPicker';
+
+const DismissKeyboard = ({children}) => (
+  <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 
 class BookingInfo extends Component {
   constructor(props){
@@ -23,6 +29,9 @@ class BookingInfo extends Component {
         adult: 0,
         children: 1,
       },
+
+      isDateTimePickerVisible: false,
+      isGenderModalVisible: false,
     }
   }
 
@@ -69,13 +78,80 @@ class BookingInfo extends Component {
     });
   }
 
+  //Các hàm quản lý DateTimePicker*
+  _showDateTimePicker = (visible) => this.setState({ isDateTimePickerVisible: visible });
+  _handleDatePicked = (date) => {
+    this._showDateTimePicker(false);
+  };
+
+  //Các hàm quản lý GenderModal
+  _showGenderModal = (visible) => this.setState({ isGenderModalVisible: visible });
+  _handleGenderModal = (isMale) => {
+    let sex = (isMale) ? 'male' : 'female';
+    this._showGenderModal(false);
+  };
+
+  // Hiển thị modal chọn giới tính
+  _renderModalContent = () => {
+    // let isMale;
+    // if (this.props.access.profile.sex != null){
+    //   isMale = (this.props.access.profile.sex.toLowerCase() == 'male') ? true : false;
+    // }
+    let isMale = true;
+
+    return(
+      <View style={styles.modalGender}>
+          <View style={{position: 'relative', flexDirection: 'row'}}>
+            <ListItem
+              style={{flex: 1,}}
+              title="Choose Gender"
+              onPress={() => {this._showGenderModal(false)}}
+              containerStyle={styles.listItemContainer}
+              rightIcon={<Icon name='close' type='FontAwesome' color='#D1D1D6'/>}
+            />
+          </View>
+          <View style={{position: 'relative', flexDirection: 'row'}}>
+            <ListItem
+              style={{flex: 1,}}
+              title="Male"
+              onPress={() => {this._handleGenderModal(true)}}
+              containerStyle={styles.listItemContainer}
+              rightIcon={isMale && <Icon name='check' type='entypo' color='#00BFFF'/>}
+            />
+          </View>
+          <View style={{position: 'relative', flexDirection: 'row'}}>
+            <ListItem
+              style={{flex: 1,}}
+              title="Female"
+              onPress={() => {this._handleGenderModal(false)}}
+              containerStyle={styles.listItemContainer}
+              rightIcon={isMale == false && <Icon name='check' type='entypo' color='#00BFFF'/>}
+            />
+          </View>
+      </View>
+    );
+  }
+
   render(){
     return(
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
+          <DateTimePicker
+            datePickerModeAndroid='spinner'
+            isVisible={this.state.isDateTimePickerVisible}
+            onConfirm={(date)=>{this._handleDatePicked(date)}}
+            onCancel={()=>{this._showDateTimePicker(false)}}
+          />
+          <Modal
+            isVisible={this.state.isGenderModalVisible}
+            onBackdropPress={()=>{this._showGenderModal(false)}}
+          >
+            {this._renderModalContent()}
+          </Modal>
+
           <BookingStage stage={1}/>
 
           <Space/>
-          
+
           <View style={styles.card}>
               <View style={styles.numberPicker}>
                   <Text style={styles.numberPickerText}>Adult *</Text>
@@ -124,10 +200,71 @@ class BookingInfo extends Component {
                   autoCorrect={false}
                   onChangeText={(value)=> this.setState({contactInfo: {address: value}})}
               />
-
           </View>
 
-      </View>
+          <InfoText text="Passenger Information"/>
+
+          <View style={styles.card}>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Fullname *"
+                  placeholderTextColor='rgba(0,0,0,0.4)'
+                  returnKeyType='next'
+                  autoCorrect={false}
+                  onChangeText={(value)=> this.setState({contactInfo: {fullname: value}})}
+              />
+              <TouchableOpacity activeOpacity={0.8} onPress={()=>this._showDateTimePicker(true)}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Birthday *"
+                    placeholderTextColor='rgba(0,0,0,0.4)'
+                    returnKeyType='next'
+                    autoCorrect={false}
+                    onChangeText={(value)=> this.setState({contactInfo: {phone: value}})}
+                    editable={false} selectTextOnFocus={false}
+                />
+              </TouchableOpacity>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Age *"
+                  placeholderTextColor='rgba(0,0,0,0.4)'
+                  keyboardType='numeric'
+                  returnKeyType='next'
+                  autoCorrect={false}
+                  onChangeText={(value)=> this.setState({contactInfo: {email: value}})}
+              />
+
+              <TouchableOpacity activeOpacity={0.8} onPress={()=>this._showGenderModal(true)}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Gender *"
+                    placeholderTextColor='rgba(0,0,0,0.4)'
+                    returnKeyType='next'
+                    autoCorrect={false}
+                    onChangeText={(value)=> this.setState({contactInfo: {address: value}})}
+                    editable={false} selectTextOnFocus={false}
+                />
+              </TouchableOpacity>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Phone number"
+                  placeholderTextColor='rgba(0,0,0,0.4)'
+                  keyboardType='phone-pad'
+                  returnKeyType='next'
+                  autoCorrect={false}
+                  onChangeText={(value)=> this.setState({contactInfo: {email: value}})}
+              />
+              <TextInput
+                  style={styles.input}
+                  placeholder="Identity number/ Passport"
+                  placeholderTextColor='rgba(0,0,0,0.4)'
+                  keyboardType='numeric'
+                  returnKeyType='next'
+                  autoCorrect={false}
+                  onChangeText={(value)=> this.setState({contactInfo: {email: value}})}
+              />
+          </View>
+      </ScrollView>
     )
   }
 }
@@ -184,6 +321,18 @@ const styles = StyleSheet.create({
       marginLeft: 20,
       color: 'gray',
       fontWeight: '500',
+    },
+    listItemContainer: {
+      height: 55,
+      borderWidth: 0.5,
+      borderColor: '#ECECEC',
+    },
+    modalGender: {
+      backgroundColor: 'white',
+      alignItems: 'center',
+      borderRadius: 4,
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+      borderWidth: 1,
     },
 })
 
