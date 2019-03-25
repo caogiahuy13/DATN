a4a03f5e266a91d96a9e6821b748b01994e2103c
your username <caogiahuy13@gmail.com>
2019-03-25 22:09:06 +0700

    LOCALIZATION: add en and en_us localize

---
+++ src/localization/en.js
@@ -0,0 +1,152 @@
+
+const en = {
+
+  name: "Name",
+  message: "Message",
+  setting: "Setting",
+  email: "Email",
+  gender: "Gender",
+  birthdate: "Birthdate",
+  address: "Address",
+  account: "Account",
+  phone: "Phone number",
+  direction: "Direction",
+  detail: "Detail",
+  description: "Description",
+  review: "Review",
+  adult: "Adult",
+  children: "Children",
+  identity: "Identity",
+  fullname: "Fullname",
+  code: "Code",
+  status: "Status",
+  order: "Order",
+  or: "Or",
+  forget: "Forget",
+
+  opening: "Opening",
+  closing: "Closing",
+
+  startDate: "Start date",
+  endDate: "End date",
+  lasting: "Lasting",
+  adultPrice: "Adult price",
+  childrenPrice: "Children price",
+  totalPrice: "Total price",
+  adultPrice: "Adult price",
+  childrenPrice: "Children price",
+
+  male: "Male",
+  female: "Female",
+  other: "Other",
+
+  historyBook: "History",
+  changePassword: "Change Password",
+  aboutUs: "About Us",
+  contact: "Contact",
+  termsCondition: "Terms Condition",
+  faq: "Faq",
+
+  congratulation: "Congratulation",
+  search: "Search",
+  checkAll: "Check all",
+  uncheckAll: "Uncheck all",
+  showMore: "Show more",
+  bookTour: "Book Tour",
+  login: "Login",
+  register: "Register",
+  emailOrPhone: "Email or phone number",
+  password: "Password",
+  lostPassword: "Lost your password",
+  loginFB: "Login with Facebook",
+  notHaveAccount: "You don't have an account", // You don't have an account?
+
+  addressModalLabel: "Enter address",
+  addressModalDescription: "Please enter your address",
+
+  newPassword: "New password",
+  oldPassword: "Old password",
+  confirmPassword: "Confirm new password",
+
+  genderMocalLabel: "Choose gender",
+
+  // Register
+  registerSuccess: "You registered an account successfully! Please check your email to verify the account!", // You registered an account successfully! Please check your email to verify the account!
+  haveAccount: "You have an account",
+
+  // Forget password
+  forgetPasswordSuccess: "New password has been sent to your email!", // New password has been sent to your email!
+  sendRequest: "Send request",
+
+  // Tour Detail
+  tourDetail: "Tour Detail",
+
+  // Passenger Information
+  passengerInfo: "Passenger Information",
+  numberOfPassengers: "Number Of Passenger",
+  contactInfo: "Contact Information",
+
+  // Payment Information
+  paymentInfo: "Payment Information",
+
+  // Confirm booking
+  confirmBooking: "Confirm your booking",
+  successBooking: "Successfully booking tour",
+
+  // Tour screen
+  allTourLoaded: "All tours have been loaded", //All tours have been loaded
+
+  // Tour search
+  selectDestination: "Select destination",
+  selectDate: "Select date",
+  maxPrice: "Max price",
+  searchReset: "Reset",
+
+  // Search Location
+  searchLocation: "Search Location",
+
+  // Filter Location
+  filterLocationType: "Filter Location Type",
+
+  // Contact
+  sendNow: "Send now",
+  sendModalLabel: "You message has been sent to our email!", // You message has been sent to our email!
+
+  // Change avatar
+  changeAvatar: "Change Avatar",
+  choosePhoto: "Choose Photo",
+
+  // Detail booked tour
+  detailBookedTour: "Detail Booked Tour Information", // Detail Booked Tour Information
+  checkoutInfo: "Checkout Information",
+  cancelTour: "Cancel tour",
+
+  // History card
+  bookingDay: "Booking day",
+  totalSlot: "Total slot",
+
+  logout: "Logout",
+
+  cancel: "Cancel",
+  ok: "OK",
+
+  ERR_USERNAME: 'Email or Phone number must be in right format',
+  ERR_PASSWORD: 'Password is required',
+  ERR_FULLNAME: 'Fullname is required',
+  ERR_NAME: 'Name is required',
+  ERR_PHONE: 'Phone number is required',
+  ERR_EMAIL: 'Email is required',
+  ERR_EMAIL_VALIDATE: 'Wrong email type',
+  ERR_CONFIRM_PASSWORD: 'Password and Confirm password must be match!',
+  ERR_PHONE_LENGTH: 'Phone number must be 10 digits!',
+  ERR_ALL_FIELD: 'All fields is required',
+  ERR_MESSAGE: 'Message is required',
+
+  ERR_BOOKING_CONTACT_INFO: 'All contact information is required',
+  ERR_BOOKING_PASSENGER_INFO: 'Fullname, Birthdate, Gender of passenger are required',
+  ERR_BOOKING_PASSENGER_MIN: 'There must be 1 passenger',
+
+  SUCCESS_CHANGE_PASSWORD: "Change Password Successfully",
+}
+
+export default en;
